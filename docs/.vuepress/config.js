9960571a16c5df6b872a0cd4842dac40d22f549a
yuzai <a545905403@163.com>
2023-05-25 10:10:16 +0800

    feat: add sum

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -190,6 +190,7 @@ module.exports = {
           '/extreme/216-实现slice',
           '/extreme/274-整数比较器',
           '/extreme/462-柯里化2',
+          '/extreme/476-Sum',
           '/extreme/todo',
         ],
         // initialOpenGroupIndex: 1 // 可选的, 默认值是 0
